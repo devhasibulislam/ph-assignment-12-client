f37c995e9ccf9e2f149d6a1bb35422ced0f47d31
hasibulislam999 <hasib143sl@gmail.com>
2022-05-23 01:33:02 +0600

    review section almost done

--- src/routes/Home/Reviews.js
+++ src/routes/Home/Reviews.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import useReviews from '../../hooks/useReviews';
 
 const Reviews = () => {
+    const [user] = useAuthState(auth);
+    const [reviews] = useReviews();
     return (
-        <div>
+        <div className='text-center lg:py-20 py-10 bg-base-200'>
+            <h1 className='flex items-baseline justify-center my-5'><i className="fa fa-hashtag text-6xl" aria-hidden="true"></i><span className='text-4xl'>Reviews of products</span></h1>
             <div class="stats shadow">
 
                 <div class="stat">
@@ -27,15 +33,39 @@ const Reviews = () => {
                     <div class="stat-figure text-secondary">
                         <div class="avatar online">
                             <div class="w-16 rounded-full">
-                                <img src="https://api.lorem.space/image/face?w=128&h=128" alt='user-avatar' />
+                                <img src={user?.photoURL} alt='user-avatar' />
                             </div>
                         </div>
                     </div>
                     <div class="stat-value">86%</div>
                     <div class="stat-title">Tasks done</div>
                     <div class="stat-desc text-secondary">31 tasks remaining</div>
+                    <div class="stat-desc text-primary">Nice job <span className='font-bold'>{user?.displayName}</span></div>
                 </div>
-
+            </div>
+            {/* review cards */}
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 container mx-auto gap-4 mt-12'>
+                {
+                    reviews.map(review => <div
+                        key={review._id}
+                        class="card bg-base-100 shadow-xl"
+                    >
+                        <figure class="px-10 pt-10">
+                            <img src={review?.reviewerAvatar} alt="reviewer" class="rounded-xl h-[426px] object-cover object-top" />
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">{review?.reviewerName}</h2>
+                            <p>{review?.reviewerSpeech}</p>
+                            <div class="rating">
+                                <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                                <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                                <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                                <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                                <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                            </div>
+                        </div>
+                    </div>)
+                }
             </div>
         </div>
     );
